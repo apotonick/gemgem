5b523c6ba845ce1571d763673a9387881662de95
Nick Sutterer <apotonick@gmail.com>
2014-05-31 17:21:34 +1000

    show status in rating cell and thing.

--- app/concepts/rating/cell.rb
+++ app/concepts/rating/cell.rb
@@ -2,12 +2,18 @@ class Rating::Cell < Cell::Concept
   property :comment
   property :created_at
   property :id
+  property :thing
 
   def show
     render
   end
 
   def status
-    link_to created_at, rating_path(id)
+    link_to created_at, rating_path(id) # DISCUSS: why not rating.url[.self]?
+  end
+
+  def thing
+    # link_to super.name, thing_path(super.id)
+    link_to model.thing.name, thing_path(model.thing.id)
   end
 end
