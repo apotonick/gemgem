4e38ae24ddf6b7b2942e022c41390f1b5cdfefc8
Nick Sutterer <apotonick@gmail.com>
2014-06-10 18:48:17 +1000

    extract form creation etc into Operation::Create, which should actually also contain Flow.

--- app/trailblazer/trailblazer/endpoint.rb
+++ app/trailblazer/trailblazer/endpoint.rb
@@ -3,18 +3,14 @@ module Trailblazer
     class Create
 
       def call(controller, params, endpoint_actions, domain=Thing) # FIXME: no dependency.
-        thing = domain::Twin.new
-
-        # TODO: no json or http stuff in here!
         is_json = controller.request.format == "application/json"
-        @form = (is_json ? domain::Operation::JSON : domain::Form).new(thing)
+        namespace = (is_json ? domain::Operation::JSON : domain::Form)
         input = is_json ? controller.request.body.string : params[domain.to_s.underscore] #params[:thing]
 
         # FIXME: don't allow instance variables in controllers?
-        controller.instance_variable_set(:@form, @form)
-
-        @form.extend(Trailblazer::Operation::Flow) # FIXME: Only for fuckin Form.
-        @form.flow(input, endpoint_actions[is_json ? :json : :form])
+        Operation::Create.new.call(domain, namespace, input,  (endpoint_actions[is_json ? :json : :form])) do |form|
+          controller.instance_variable_set(:@form, form) # TODO: remove.
+        end
       end
 
     private
