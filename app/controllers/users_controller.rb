6005682b45818c234db6eef6bb137112f6cdb809
Nick Sutterer <apotonick@gmail.com>
2014-09-21 18:31:28 +1000

    add before_filter to validate confirmation_token.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,7 +6,14 @@ class UsersController < ApplicationController
     # render json: [{"label"=>"mylabel","value"=>"myvalue"}]
   end
 
+  before_filter :is_confirm_allowed?, only: :confirm
   def confirm
     @form = User::Operation::Confirm.contract(params)
+  end
+
+private
+  def is_confirm_allowed?
+    require 'monban_extensions'
+    raise unless Monban::IsConfirmationAllowed[params]
   end
 end
